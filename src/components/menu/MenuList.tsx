db33a228cefb75034d1181a3139b59268a1fc37a
_RynnLee <38906839+Rynn-Lee@users.noreply.github.com>
2023-04-18 23:38:43 +0600

    Little design changes
    Moved dish titles, cost and category.
    + some mobile adaptiveness

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -5,12 +5,12 @@ export default function MenuList({menu}: any){
     <div className="menu-body">
       {menu.map((item: any, index: number)=>(
         <div className="bg-3 horizontal menu-card" key={index}>
-          <Image src={"/images/" + item.filename} alt="image" width={300} height={300} className="round-left img-fill"/>
+          <Image src={"/images/" + item.filename} alt="image" width={300} height={300} className="img-fill"/>
           <div className="vertical menu-card-info">
             <div className="name">{item.name}</div>
-            <div className="category">{item.category}</div><hr/>
+            <div className="category"></div><hr/>
             <div className="description">{item.description}</div>
-            <div className="cost">{item.cost} тг.</div>
+            <div className="cost"><span>{item.category} • </span>{item.cost} тг.</div>
           </div>
         </div>
       ))}
