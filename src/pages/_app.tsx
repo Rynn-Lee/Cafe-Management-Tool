ea5a3b9ff9ba5704e12c638c8fde8a1ca3b00a42
_RynnLee <38906839+Rynn-Lee@users.noreply.github.com>
2023-04-21 20:54:47 +0600

    Installed React-Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,29 @@ import { store } from '@/store/store'
 import { Provider } from 'react-redux'
 import { useEffect } from 'react'
 import { services } from '@/services'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) { 
   const connect = async() => await services.db.connect()
   useEffect(()=>{connect()}, [])
 
   return(
-  <Provider store={store}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+    <ReactQueryDevtools/>
+  </QueryClientProvider>
   )
 }
